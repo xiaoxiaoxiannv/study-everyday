85adf43586812e09ef082a2b62a142e6580cf06a
Luna <1017929760@qq.com>
2020-12-16 22:58:23 +0800

    请求friends.json

--- public/qq.js
+++ public/qq.js
@@ -1,1 +1,8 @@
-console.log('qq')
+const request = new XMLHttpRequest()
+request.open('GET','/friends.json')
+request.onreadystatechange=()=>{
+    if(request.readyState===4 && request.status===200){
+        console.log(request.response)
+    }
+}
+request.send()
