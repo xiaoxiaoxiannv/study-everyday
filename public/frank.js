103c897d7fce56a9c0ee4a4de06f192ed00f742c
Luna <1017929760@qq.com>
2020-12-16 22:58:47 +0800

    请求qq.com的friends.json

--- public/frank.js
+++ public/frank.js
@@ -1,1 +1,8 @@
-console.log('黑客')
+const request = new XMLHttpRequest()
+request.open('GET','http://qq.com:8888/friends.json')
+request.onreadystatechange=()=>{
+    if(request.readyState===4 && request.status===200){
+        console.log(request.response)
+    }
+}
+request.send()
