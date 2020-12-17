62118c355361bcb4981dd390f985553f98744ff5
Luna <1017929760@qq.com>
2020-12-17 17:02:54 +0800

    jsonp用referer定向分享

--- server.js
+++ server.js
@@ -40,13 +40,18 @@ var server = http.createServer(function (request, response) {
         response.write(fs.readFileSync('./public/friends.json'))
         response.end()
     } else if (path === '/friends.js') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
-        const string = fs.readFileSync('./public/friends.js').toString()
-        const data = fs.readFileSync('./public/friends.json').toString()
-        const string2 = string.replace("{{data}}", data)
-        response.write(string2)
-        response.end()
+        if(request.headers["referer"].indexOf("http://frank.com:9999")===0){
+            response.statusCode = 200
+            response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+            const string = fs.readFileSync('./public/friends.js').toString()
+            const data = fs.readFileSync('./public/friends.json').toString()
+            const string2 = string.replace("{{data}}", data)
+            response.write(string2)
+            response.end()
+        }else {
+            response.statusCode = 404
+            response.end()
+        }
     } else {
         response.statusCode = 404
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
