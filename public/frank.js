309a99cc6a2741f06d1dee360cc129bd01ba6bbd
Luna <1017929760@qq.com>
2020-12-17 17:27:11 +0800

    封装jsonp

--- public/frank.js
+++ public/frank.js
@@ -1,8 +1,21 @@
-const random = 'frankJSONPCallbackName' + Math.random()
-console.log(random)
-window[random] = (data)=>{
-    console.log(data)
+function jsonp(url){
+    return new Promise((resolve,reject)=>{
+        const random = 'frankJSONPCallbackName' + Math.random()
+        window[random] = (data)=>{
+            resolve(data)
+        }
+        const script = document.createElement('script')
+        script.src = `${url}?callback=${random}`
+        script.onload =()=>{
+            script.remove()
+        }
+        script.onerror=()=>{
+            reject()
+        }
+        document.body.appendChild(script)
+    })
 }
-const script = document.createElement('script')
-script.src = `http://qq.com:8888/friends.js?functionName=${random}`
-document.body.appendChild(script)
+jsonp('http://qq.com:8888/friends.js').then((data)=>{
+    console.log(data)
+})
+
