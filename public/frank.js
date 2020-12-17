7d404e6c380b48c70e51229c5eacb069da81a602
Luna <1017929760@qq.com>
2020-12-17 17:12:35 +0800

    防止占用全局变量,使用随机数

--- public/frank.js
+++ public/frank.js
@@ -1,8 +1,8 @@
-const request = new XMLHttpRequest()
-request.open('GET','http://qq.com:8888/friends.json')
-request.onreadystatechange=()=>{
-    if(request.readyState===4 && request.status===200){
-        console.log(request.response)
-    }
+const random = 'frankJSONPCallbackName' + Math.random()
+console.log(random)
+window[random] = (data)=>{
+    console.log(data)
 }
-request.send()
+const script = document.createElement('script')
+script.src = `http://qq.com:8888/friends.js?functionName=${random}`
+document.body.appendChild(script)
